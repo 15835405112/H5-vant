4c75a1b504ee9fe68ef6e24bce2a49cbb38f239b
duancaixiang <duancx17@163.com>
2022-06-22 00:29:58 +0800

    总资产

--- src/views/test1/pages/property/index.vue
+++ src/views/test1/pages/property/index.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class='AppBodyBox'>
-    property
-  </div>
+  <van-tabs v-model="active" class="AppBodyBox">
+    <van-tab title="总资产" name="1">
+      <TotalAssets></TotalAssets>
+      111
+    </van-tab>
+    <van-tab title="总负债" name="2">
+      <TotalLiability></TotalLiability>
+    </van-tab>
+    <van-tab title="净资产" name="3">
+      <NetAssets></NetAssets>
+    </van-tab>
+  </van-tabs>
 </template>
 
 <script>
-
+import TotalAssets from './components/TotalAssets'
+import TotalLiability from './components/TotalLiability'
+import NetAssets from './components/NetAssets'
 export default {
-  components: {},
+  components: { TotalAssets, TotalLiability, NetAssets },
   data () {
     return {
-
+      active: '1'
     };
   },
-  computed: {},
+  computed: {
+  },
   watch: {},
   methods: {
 
@@ -21,4 +33,13 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
+.AppBodyBox /deep/ .van-tabs__content {
+  width: 100%;
+  height: 94%;
+}
+
+.AppBodyBox /deep/ .van-tab__pane {
+  width: 100%;
+  height: 100%;
+}
 </style>
